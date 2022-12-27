5fb4e3d3a12e4253978bbb269d6be9b4344190b7
swan0sasha <swan.sasha@gmail.com>
2022-12-28 02:31:31 +0700

    add generate button, explanation about colors, default map size

--- core/src/com/mygdx/td/view/actors/LeftBar.java
+++ core/src/com/mygdx/td/view/actors/LeftBar.java
@@ -2,14 +2,18 @@ package com.mygdx.td.view.actors;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.mygdx.td.view.StageManagement;
 import com.mygdx.td.view.actors.buttons.Effects;
 import com.mygdx.td.view.actors.buttons.Entities;
+import com.mygdx.td.view.actors.buttons.Generate;
 import com.mygdx.td.view.actors.buttons.Map;
 import com.mygdx.td.view.utils.Constants;
 
 public class LeftBar {
-    Group group;
-    public LeftBar() {
+    private Group group;
+    public StageManagement stageManagement;
+    public LeftBar(StageManagement stageManagement) {
+        this.stageManagement = stageManagement;
         group = new Group();
         Rectangle settingRec = new Rectangle(0, 0,
                 Constants.SETTING_WINDOW_X - 2 * Constants.SETTING_WINDOW_Y,
@@ -19,15 +23,19 @@ public class LeftBar {
         Entities entities = new Entities("Entities", this);
         Map map = new Map("Map", this);
         Effects effects = new Effects("Effects", this);
+        Generate generate = new Generate("Generate", this);
         entities.getButton().setPosition((settingRec.getWidth() - entities.getButton().getWidth()) / 2,
                 Constants.APP_HEIGHT - Constants.SETTING_WINDOW_Y * 4);
         map.getButton().setPosition((settingRec.getWidth() - map.getButton().getWidth()) / 2,
                 Constants.APP_HEIGHT - Constants.SETTING_WINDOW_Y * 6);
         effects.getButton().setPosition((settingRec.getWidth() - effects.getButton().getWidth()) / 2,
                 Constants.APP_HEIGHT - Constants.SETTING_WINDOW_Y * 8);
+        generate.getButton().setPosition((settingRec.getWidth() - effects.getButton().getWidth()) / 2,
+                Constants.SETTING_WINDOW_Y);
         group.addActor(entities.getButton());
         group.addActor(map.getButton());
         group.addActor(effects.getButton());
+        group.addActor(generate.getButton());
         group.setPosition(Constants.SETTING_WINDOW_Y, Constants.SETTING_WINDOW_Y);
     }
 
--- core/src/com/mygdx/td/view/actors/MapEditor.java
+++ core/src/com/mygdx/td/view/actors/MapEditor.java
@@ -2,38 +2,79 @@ package com.mygdx.td.view.actors;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.mygdx.td.TowerDefence;
+import com.mygdx.td.view.StageManagement;
 import com.mygdx.td.view.actors.buttons.CreateMap;
 import com.mygdx.td.view.utils.Constants;
+import com.sun.tools.javac.util.StringUtils;
 
 public class MapEditor {
-    TowerDefence towerDefence;
+    StageManagement stageManagement;
     private Group group;
     private Group map;
     TextField mapWidth;
     TextField mapHeight;
-    public MapEditor(TowerDefence towerDefence){
-        this.towerDefence = towerDefence;
+    public MapEditor(StageManagement stageManagement){
+        this.stageManagement = stageManagement;
         group = new Group();
         Rectangle settingRec = new Rectangle(0, 0,
                 Constants.SETTING_WINDOW_WIDTH, Constants.SETTING_WINDOW_HEIGHT, Color.GRAY);
         group.addActor(settingRec);
+        Label textWidth = new Label("Width", TowerDefence.appSkin);
+        textWidth.setColor(Color.BLACK);
+        textWidth.setPosition(Constants.SETTING_WINDOW_WIDTH - 200, Constants.SETTING_WINDOW_HEIGHT - 45);
+        group.addActor(textWidth);
         mapWidth = new TextField("", TowerDefence.appSkin);
         mapWidth.setPosition(Constants.SETTING_WINDOW_WIDTH - 200, Constants.SETTING_WINDOW_HEIGHT - 100);
         group.addActor(mapWidth);
+        Label textHeight = new Label("Height", TowerDefence.appSkin);
+        textHeight.setColor(Color.BLACK);
+        textHeight.setPosition(Constants.SETTING_WINDOW_WIDTH - 200, Constants.SETTING_WINDOW_HEIGHT - 145);
+        group.addActor(textHeight);
         mapHeight = new TextField("", TowerDefence.appSkin);
         mapHeight.setPosition(Constants.SETTING_WINDOW_WIDTH - 200, Constants.SETTING_WINDOW_HEIGHT - 200);
         group.addActor(mapHeight);
         CreateMap createMap = new CreateMap("Create", this);
         createMap.getButton().setPosition(Constants.SETTING_WINDOW_WIDTH - 200, Constants.SETTING_WINDOW_HEIGHT - 300);
         group.addActor(createMap.getButton());
+        Label green = new Label("Green: empty", TowerDefence.appSkin);
+        green.setColor(Color.BLACK);
+        Label yellow = new Label("Yellow: path", TowerDefence.appSkin);
+        yellow.setColor(Color.BLACK);
+        Label gray = new Label("Gray: towers", TowerDefence.appSkin);
+        gray.setColor(Color.BLACK);
+        Label blue = new Label("Blue: base", TowerDefence.appSkin);
+        blue.setColor(Color.BLACK);
+        Label red = new Label("Red: spawn", TowerDefence.appSkin);
+        red.setColor(Color.BLACK);
+        green.setPosition(Constants.SETTING_WINDOW_WIDTH - 270, Constants.SETTING_WINDOW_HEIGHT - 400);
+        yellow.setPosition(Constants.SETTING_WINDOW_WIDTH - 270, Constants.SETTING_WINDOW_HEIGHT - 450);
+        gray.setPosition(Constants.SETTING_WINDOW_WIDTH - 270, Constants.SETTING_WINDOW_HEIGHT - 500);
+        blue.setPosition(Constants.SETTING_WINDOW_WIDTH - 270, Constants.SETTING_WINDOW_HEIGHT - 550);
+        red.setPosition(Constants.SETTING_WINDOW_WIDTH - 270, Constants.SETTING_WINDOW_HEIGHT - 600);
+        group.addActor(green);
+        group.addActor(yellow);
+        group.addActor(gray);
+        group.addActor(blue);
+        group.addActor(red);
         group.setPosition(Constants.SETTING_WINDOW_X, Constants.SETTING_WINDOW_Y);
     }
 
     public void addMap(){
-        int width = Integer.parseInt(mapWidth.getText());
-        int height = Integer.parseInt(mapHeight.getText());
+        int width;
+        int height;
+        try {
+            width = Integer.parseInt(mapWidth.getText());
+        } catch (NumberFormatException nfe){
+            width = 10;
+        }
+        try {
+            height = Integer.parseInt(mapHeight.getText());
+        } catch (NumberFormatException nfe){
+            height = 10;
+        }
         //LevelMap создать пустую карту по ширине высоте
         map = new Group();
         for (int i = 0; i < width * Constants.TILE_SIZE; i = i + Constants.TILE_SIZE){
@@ -44,7 +85,7 @@ public class MapEditor {
         }
         map.setPosition(Constants.TILE_SIZE, Constants.TILE_SIZE);
         group.addActor(map);
-        towerDefence.controller.getMapController().addMap(width, height);
+        stageManagement.towerDefence.controller.getMapController().addMap(width, height);
     }
     public Group getGroup() {
         return group;
